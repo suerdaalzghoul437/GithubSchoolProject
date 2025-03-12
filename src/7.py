9a92f900781938260d707059523a7fda128d94b9
suerdaalzghoul437 <suerdaalzghoul437@gmail.com>
2025-03-12 02:31:02 +0000

    Add 7.py

---
+++ src/7.py
@@ -0,0 +1,5 @@
+import random
+
+def generate_random_code(length):
+    chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    return ''.join(random.choice(chars) for _ in range(length))
